a55c4a4c68542d9f0428f91cacdb8c49ed4fb6d3
leonardoricci-tsi <lo5642050@gmail.com>
2025-08-12 01:42:31 +0000

    Aula 11/08/2025

---
+++ src/main/java/app/Aluno.java
@@ -0,0 +1,6 @@
+package app;
+
+public class Aluno {
+    String nome;  //Tenho que definir cada tipo de variavel(int,String,Double) em cada variavél.
+    int idade;
+}
---
+++ src/main/java/app/PrimeiraClasse.java
@@ -0,0 +1,7 @@
+package app; //colocar naonde está a aplicação java
+
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!!");
+    }
+}
